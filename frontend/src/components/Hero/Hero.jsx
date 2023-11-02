6c47c087acd941ae3434aa7974e03fe07622e4c5
Snezoh97 <sinelizwitaku@gmail.com>
2023-11-02 11:12:52 +0200

    Apply Button

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -15,12 +15,12 @@ const Hero = () => {
       </div>
       <div className="absolute inset-0 bg-black opacity-80"></div>
       <div className="absolute w-full inset-0 py-20 text-white ">
-        <div className="md:max-w-[1480px] m-auto grid md:grid-cols-2 gap-10 max-w-[540px]">
+        <div className="md:max-w-[1480px] m-auto grid md:grid-cols-2 gap-4 max-w-[540px]">
           <div className="flex flex-col justify-start gap-4">
             <h1 className="py-2 text-4xl text-[#9FF3C1] font-bold">
               Bridging Futures, Creating Opportunities
             </h1>
-            <p className="md:leading-[44px] py-10 md:text-2xl text-1xl font-medium">
+            <p className="md:leading-[44px] py-4 md:text-2xl text-1xl font-medium">
               <span className="text-[#9FF3C1]">Welcome to EduConnect,</span>{" "}
               where we are more than just an educational platform; <br></br>
               We are architects of futures and creators of opportunities. Our
@@ -31,11 +31,13 @@ const Hero = () => {
           </div>
           <img src={heroImg} className="rounded-lg h-[auto] " />
         </div>
-        <div>
-          <button className="border border-[#47DEFF] flex jusify-between items-center px-2 py-2 rounded-md bg-black text-white gap-2 font-bold">
-            <img src={student} />
-            Student
-          </button>
+        <div className="w-full">
+          <div className="md:max-w-[1480px] max-w-[540px] m-auto py-20 flex justify-center">
+            <button className="border border-[#47DEFF] flex items-center px-2 py-2 rounded-md bg-black text-white gap-2 font-bold">
+              <img src={student} />
+              Apply!
+            </button>
+          </div>
         </div>
       </div>
     </div>
